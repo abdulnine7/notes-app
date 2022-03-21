f92c8e13be5709b4da781da7d01ae9c727ddae30
abdulnine7 <me@abdul.co.in>
2022-03-21 22:14:43 +0530

    ui changes

--- js/script.js
+++ js/script.js
@@ -10,15 +10,15 @@ addBtn.addEventListener('click', () => addNewNote())
 
 function addNewNote(text = '') {
     const note = document.createElement('div')
-    note.classList.add('note')
+    note.classList.add("note", "card", "m-2", "cursor-pointer", "rounded-lg", "hover:shadow-md", "hover:border-opacity-0", "transform", "hover:-translate-y-1", "transition-all", "duration-200", "bg-white", "h-96", "overflow-y-scroll", "overflow-x-hidden", "no-scrollbar", "dark:bg-gray-800", "dark:border-gray-700", "dark:text-white")
 
     note.innerHTML = `
-    <div class="tools">
+    <div class="tools text-lg text-slate-300 bg-stone-800 rounded-t-lg px-2 py-1 align-top flex justify-end space-x-3 shadow-lg dark:bg-gray-300 dark:text-slate-900">
         <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
         <button class="delete"><i class="fa-solid fa-trash-can"></i></i></button>
     </div>
-    <div class="main ${text ? "" : "hidden"}"></div>
-    <textarea class="${text ? "hidden" : ""}"></textarea>
+    <div class="main ${text ? "" : "hidden"} w-full h-full p-2"></div>
+    <textarea class="${text ? "hidden" : ""} w-full h-full p-2 dark:bg-gray-800 dark:text-white" placeholder="Write here..."></textarea>
     `
 
     const editBtn = note.querySelector('.edit')
@@ -27,7 +27,7 @@ function addNewNote(text = '') {
     const textArea = note.querySelector('textarea')
 
     textArea.value = text
-    main.innerHTML = marked(text)
+    main.innerHTML =  marked.parse(text)
 
     deleteBtn.addEventListener('click', () => {
         note.remove()
@@ -43,12 +43,12 @@ function addNewNote(text = '') {
     textArea.addEventListener('input', (e) => {
         const { value } = e.target
 
-        main.innerHTML = marked(value)
+        main.innerHTML = marked.parse(value)
 
         updateLS()
     })
 
-    document.body.appendChild(note)
+    document.getElementById('note-container').appendChild(note)
 }
 
 function updateLS() {
