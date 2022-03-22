7fa638702785ff4789c411bda54c2046772399f8
abdulnine7 <me@abdul.co.in>
2022-03-22 23:43:41 +0530

    theme fixes and edit btn

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ addBtn.addEventListener('click', () => addNewNote())
 
 function addNewNote(text = '') {
     const note = document.createElement('div')
-    note.classList.add("note", "card", "m-6", "cursor-pointer", "rounded-lg", "hover:shadow-md", "hover:border-opacity-0", "transform", "hover:-translate-y-1", "transition-all", "duration-200", "bg-white", "bg-opacity-80" ,"h-96", "overflow-y-scroll", "overflow-x-hidden", "no-scrollbar", "dark:bg-gray-800", "dark:border-gray-700", "dark:text-white")
+    note.classList.add("note", "card", "m-6", "cursor-pointer", "rounded-lg", "hover:shadow-md", "hover:border-opacity-0", "transform", "hover:-translate-y-1", "transition-all", "duration-200", "bg-white", "bg-opacity-80" ,"h-96", "overflow-y-scroll", "overflow-x-hidden", "no-scrollbar", "dark:bg-gray-800", "dark:bg-opacity-80", "dark:border-gray-700", "dark:text-white")
 
     note.innerHTML = `
-    <div class="tools text-lg text-slate-300 bg-stone-800 rounded-t-lg px-2 py-1 align-top flex justify-end space-x-3 shadow-lg dark:bg-gray-300 dark:text-slate-900">
-        <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
-        <button class="delete"><i class="fa-solid fa-trash-can"></i></i></button>
+    <div class="tools sticky top-0 z-10 text-lg text-slate-300 bg-stone-800 rounded-t-lg px-2 py-1 align-top flex justify-end space-x-2 shadow-lg dark:bg-gray-300 dark:text-slate-900">
+        <button class="edit ${text ? "" : "bg-red-600 dark:bg-red-400"} rounded-md"><i class="fa-solid fa-pen-to-square px-1.5" title="Edit/Lock"></i></button>
+        <button class="delete rounded-md" title="Delete"><i class="fa-solid fa-trash-can px-1.5"></i></i></button>
     </div>
     <div class="main ${text ? "" : "hidden"} w-full h-full p-2"></div>
     <textarea class="${text ? "hidden" : ""} w-full h-full p-2 dark:bg-gray-800 dark:text-white" placeholder="Write here..."></textarea>
@@ -38,6 +38,8 @@ function addNewNote(text = '') {
     editBtn.addEventListener('click', () => {
         main.classList.toggle('hidden')
         textArea.classList.toggle('hidden')
+        editBtn.classList.toggle('bg-red-600')
+        editBtn.classList.toggle('dark:bg-red-400')
     })
 
     textArea.addEventListener('input', (e) => {
--- js/theme.js
+++ js/theme.js
@@ -4,14 +4,17 @@ var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     themeToggleLightIcon.classList.remove('hidden');
+    themeToggleDarkIcon.classList.add('hidden');
+    document.documentElement.classList.add('dark');
 } else {
+    themeToggleLightIcon.classList.add('hidden');
     themeToggleDarkIcon.classList.remove('hidden');
+    document.documentElement.classList.remove('dark');
 }
 
 var themeToggleBtn = document.getElementById('theme-toggle');
 
 themeToggleBtn.addEventListener('click', function() {
-  console.log("Theme change...");
     // toggle icons inside button
     themeToggleDarkIcon.classList.toggle('hidden');
     themeToggleLightIcon.classList.toggle('hidden');
